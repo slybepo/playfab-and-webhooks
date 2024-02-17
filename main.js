bd820bf33d227baf6cc1c4672f63be40390a0492
slybepo <155332027+slybepo@users.noreply.github.com>
2024-02-17 10:59:23 +0100

    Create main.js
    The scripr

---
+++ main.js
@@ -0,0 +1,8 @@
+var contentBody = {
+    "content": "Hello " + currentPlayerId+ " ! " // your text
+};
+var url = "";// the webhook url
+var method = "post";
+var contentType = "application/json";
+var headers = {};
+var responseString =  http.request(url,method,JSON.stringify(contentBody),contentType,headers);
