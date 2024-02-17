b4f2e746914d9964fd2fc8bac235779205f3164b
slybepo <155332027+slybepo@users.noreply.github.com>
2024-02-17 11:07:40 +0100

    Main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+handlers.hello = function(args) {
 var contentBody = {
     "content": "Hello " + currentPlayerId+ " ! " // your text
 };
@@ -6,3 +7,4 @@ var method = "post";
 var contentType = "application/json";
 var headers = {};
 var responseString =  http.request(url,method,JSON.stringify(contentBody),contentType,headers);
+}
